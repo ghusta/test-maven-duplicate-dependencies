5ab0f2e86805aec2291953103aff7a88aaf74dd8
Guillaume HUSTA <guillaume.husta@gmail.com>
2026-06-24 08:06:56 +0200

    style: Use Bootstrap 5.3

--- src/main/java/org/example/servlets/ClassFinderServlet.java
+++ src/main/java/org/example/servlets/ClassFinderServlet.java
@@ -28,20 +28,31 @@ public class ClassFinderServlet extends HttpServlet {
         var out = response.getWriter();
 
         out.println("""
-                <html>
+                <!doctype html>
+                <html lang="fr">
                 <head>
-                  <title>Class Finder</title>
-                </head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <title>Class Finder</title>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">                </head>
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
                 <body>
+                <div class="container" id="main">
                 <h2>Recherche de classe dans le classloader</h2>
                 
-                <form method="get">
-                  FQCN :
-                  <input type="text" name="fqcn" size="60" autofocus/>
-                  <input type="submit" value="Chercher"/>
+                <form method="get" class="mt-3">
+                    <div class="mb-3">
+                        <label for="fqcn" class="form-label">FQCN :</label>
+                        <input type="text" class="form-control" name="fqcn" size="60" autofocus id="fqcn"/>
+                    </div>
+                    <div class="mb-3">
+                        <input type="submit" value="Chercher" class="btn btn-primary"/>
+                    </div>
                 </form>
                 
-                <b>Version de Java Runtime : %s</b><br/>
+                <div class="mt-3 alert alert-info" role="alert">
+                    <b><i class="bi bi-info-circle me-2"></i>Version de Java Runtime : %s</b>
+                </div>
                 <hr/>
                 """ .formatted(System.getProperty("java.version")));
 
@@ -49,7 +60,7 @@ public class ClassFinderServlet extends HttpServlet {
 
             String resource = fqcn.replace('.', '/') + ".class";
 
-            out.println("<b>Recherche :</b> " + resource + "<br/><br/>");
+            out.println("<b><i class=\"bi bi-search me-2 \"></i>Recherche :</b> " + resource + "<br/><br/>");
 
             ClassLoader cl = Thread.currentThread().getContextClassLoader();
 
@@ -65,7 +76,7 @@ public class ClassFinderServlet extends HttpServlet {
 
                 URL url = urls.nextElement();
 
-                out.println("<pre>");
+                out.println("<pre style=\"white-space: pre-wrap;\">");
                 out.println("URL : " + url);
 
                 if ("jar".equals(url.getProtocol())) {
@@ -83,11 +94,18 @@ public class ClassFinderServlet extends HttpServlet {
                 out.println("<b>Aucun résultat</b>");
             }
             if (occurences > 1) {
-                out.println("<b>⚠️ Plusieurs occurences trouvées</b>");
+                out.println("""
+                        <div class="alert alert-warning" role="alert">
+                          <b><span class="me-2">⚠️</span>Plusieurs occurences trouvées</b>
+                        </div>
+                        """);
             }
         }
 
         out.println("""
+                </div>
+                <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script>
                 </body>
                 </html>
                 """);
